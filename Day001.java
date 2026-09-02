db188dc327ef6b0099ea1054d2bad08c80428169
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2026-09-02 22:55:02 +0800

    Create Day001.java

---
+++ Day001.java
@@ -0,0 +1,10 @@
+public class day001 {
+    public static void main(String[] args) {
+        //ini fungsi nya untuk komentar 1 baris
+        /*ini fungsinya untuk komentar beberapa baris*/
+        /**ini fungsinya untuk beberapa baris tetapi biasanya untuk main**/
+
+        System.out.println("Hello World");
+    }
+    
+}
