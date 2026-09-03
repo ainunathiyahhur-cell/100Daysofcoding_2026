0b4589caa26271a36e082bbf3625921d6a3b4e39
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2026-09-03 20:14:03 +0800

    Create day002.java

---
+++ day002.java
@@ -0,0 +1,21 @@
+public class day002 {
+    public static void main(String[] args) {
+        String nama = "Athiyah";
+        String nim = "D0225510";
+        int umur = 19;
+        String jurusan = "INFORMATIKA";
+
+        System.out.println("====================");
+        System.out.println("|   BIODATA DIRI    |");
+        System.out.println("====================");
+        System.out.println("Nama    : " + nama);
+
+        System.out.printf("NIM     : %s%n", nim);
+        System.out.printf("Umur    : %d tahun%n", umur);
+
+        System.out.print("Jurusan : ");
+        System.out.println(jurusan);
+     
+
+    }
+}
