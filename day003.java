e011ae0231153e21581ef7471b54397be46f9454
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2026-09-04 19:09:51 +0800

    Create day003.java

---
+++ day003.java
@@ -0,0 +1,18 @@
+public class day003 {
+    public static void main(String[] args) {
+        System.out.println("===================================");
+        System.out.println("\tTOKO BUKU: \"GARUDA\"");
+        System.out.println("===================================");
+        System.out.println("Promo hari ini: \"Beli 2 gratis 1\"\n");
+
+        System.out.println("Item\t harga");
+        System.out.println("Novel\t Rp 55.000");
+        System.out.println("Komik\t Rp 25.000");
+        System.out.println("Atlas\t Rp 80.000\n");
+
+        System.out.println("File struk disimpan di: D:\\Toko\\Struk");
+        System.out.println("Terima kasih sudah belanja di Toko GARUDA");
+    }
+
+    
+}
