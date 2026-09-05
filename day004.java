bbda1908de3b17e8fd346d35b64338e65f98f89d
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2026-09-05 20:09:55 +0800

    Create day004.java

---
+++ day004.java
@@ -0,0 +1,14 @@
+public class day004 {
+    public static void main(String[] args) {
+        byte umur = 19;
+        short tahun = 2026;
+        int jumlahPenduduk = 100000;
+        long jarak = 10000000000L;
+
+        System.out.println("Umur = " + umur);
+        System.out.println("Tahun = " + tahun);
+        System.out.println("Jumlah Penduduk = " + jumlahPenduduk);
+        System.out.println("Jarak = " + jarak);
+    }
+
+}
