4363be594158970411d8dc5ffc52961017ea396f
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2026-09-06 21:04:43 +0800

    Create day005.java

---
+++ day005.java
@@ -0,0 +1,12 @@
+public class day005 {
+    public static void main(String[] args) {
+        double harga = 12000.75;
+        float nilai = 90.5f;
+        
+        System.out.println("Harga = " + harga);
+        System.out.println("Nilai = " + nilai);
+
+        //double menyimpan harga dengan pecahan
+        //float menyimpan tinggi dengan pecahan
+    }
+}
