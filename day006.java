da2965774145da3185280412db157763e034a8ae
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2026-09-07 20:11:05 +0800

    Create day006.java

---
+++ day006.java
@@ -0,0 +1,12 @@
+public class day006 {
+    public static void main(String[] args) {
+        //Menyimpan satu karakter
+        char huruf = 'A';
+
+        //Menyimpan nilai benar atau salah
+        boolean lulus = true;
+
+        System.out.println("Huruf = " + huruf);
+        System.out.println("Lulus = " + lulus);
+    }
+}
