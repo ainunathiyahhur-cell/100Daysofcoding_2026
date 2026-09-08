9b51fdecec2d3898973569b829ca2bbfc00ce879
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2026-09-08 20:59:12 +0800

    Create day007.java

---
+++ day007.java
@@ -0,0 +1,9 @@
+public class day007 {
+
+    public static void main(String[] args) {
+        //String digunakan untuk menyimpan teks
+        String nama = "Athiyah";
+
+        System.out.println("Nama saya = " + nama);
+    }
+}
