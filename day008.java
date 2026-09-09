e5b3a61f4f7638a6f547a15993fa8b21af0c5d84
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2026-09-09 22:46:23 +0800

    Create day008.java

---
+++ day008.java
@@ -0,0 +1,14 @@
+public class day008 {
+
+    public static void main(String[] args) {
+        //deklarasi 
+        String nama;
+        //inisialisasi
+        nama = "athiyah";
+
+        System.out.println("Nama awal = " + nama);
+        //update
+        nama = "ainun";
+        System.out.println("Nama setelah diupdate: " + nama);
+    }
+}
