5843de9591223ce667e0ac0ac04072717633a2f1
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2026-09-11 22:29:03 +0800

    Create day10.java

---
+++ day10.java
@@ -0,0 +1,20 @@
+public class day10 {
+    public static void main(String[] args) {
+        // Menampilkan batas minimal dan maksimal tipe data
+        System.out.println("BYTE");
+        System.out.println("Minimal : " + Byte.MIN_VALUE);
+        System.out.println("Maksimal: " + Byte.MAX_VALUE);
+
+        System.out.println("\nSHORT");
+        System.out.println("Minimal : " + Short.MIN_VALUE);
+        System.out.println("Maksimal: " + Short.MAX_VALUE);
+
+        System.out.println("\nINT");
+        System.out.println("Minimal : " + Integer.MIN_VALUE);
+        System.out.println("Maksimal: " + Integer.MAX_VALUE);
+
+        System.out.println("\nLONG");
+        System.out.println("Minimal : " + Long.MIN_VALUE);
+        System.out.println("Maksimal: " + Long.MAX_VALUE);
+    }
+}
