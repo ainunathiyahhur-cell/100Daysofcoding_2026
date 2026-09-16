dbd77b96815b2071c83f01fb2a94c77de1f9c0f4
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2026-09-16 23:11:18 +0800

    Create day15.java

---
+++ day15.java
@@ -0,0 +1,14 @@
+public class day15 {
+
+    public static void main(String[] args) {
+
+        double hargaPizza = 180000;
+        double jumlahOrang = 4;
+
+        double bayarMasingMasing = hargaPizza / jumlahOrang;
+
+        System.out.println("Harga pizza: Rp" + hargaPizza);
+        System.out.println("Jumlah orang: " + jumlahOrang);
+        System.out.println("Bayar masing-masing: Rp" + bayarMasingMasing);
+    }
+}
