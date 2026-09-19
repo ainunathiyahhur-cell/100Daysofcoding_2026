a73a753ccd6a2b05d3b1ae711f72622e9fc91b08
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2026-09-19 22:47:58 +0800

    Create day18.java

---
+++ day18.java
@@ -0,0 +1,11 @@
+public class day18 {
+    public static void main(String[] args) {
+
+        int angka = 10;
+
+        double hasil = angka;
+
+        System.out.println("Nilai int: " + angka);
+        System.out.println("Nilai double: " + hasil);
+    }
+}
