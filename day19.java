963de2b5dc164d5ec313a520c9a3678093d902e2
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2026-09-20 22:35:36 +0800

    Create day19.java

---
+++ day19.java
@@ -0,0 +1,12 @@
+
+public class day19 {
+    public static void main(String[] 
+                            
+        double angkaBesar = 10.75;
+
+        int angkaKecil = (int) angkaBesar;
+  
+        System.out.println("Nilai double: " + angkaBesar);
+        System.out.println("Nilai int: " + angkaKecil);
+    }
+}
