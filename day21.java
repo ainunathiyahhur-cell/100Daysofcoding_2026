2b5d55ee4950c7091ef564525f0cfe0aa53a957a
ainunathiyahhur-cell <ainunathiyahhur@gmail.com>
2026-09-22 21:07:34 +0800

    Create day21.java

---
+++ day21.java
@@ -0,0 +1,17 @@
+public class day21 {
+
+    public static void main(String[] args) {
+
+        String umurString = "19";
+        String ipkString = "3.75";
+        String nilaiString = "85";
+if
+        int umur = Integer.parseInt(umurString);
+        double ipk = Double.parseDouble(ipkString);
+        int nilai = Integer.parseInt(nilaiString);
+
+        System.out.println("Umur: " + umur);
+        System.out.println("IPK: " + ipk);
+        System.out.println("Nilai: " + nilai);
+    }
+}
